782074628300a3ca0367ae04e568df55e5cf0ad1
srinivas191 <142072902+srinivas191@users.noreply.github.com>
2024-11-11 15:42:23 +0530

    solved

--- program1.js
+++ program1.js
@@ -1,7 +1,3 @@
-/**
- * @param {string} s
- * @return {boolean}
- */
 var isValid = function(s) {
     const stack = [];
     const map = {
@@ -9,15 +5,20 @@ var isValid = function(s) {
         '{': '}',
         '[': ']'
     };
+
     for (let char of s) {
+        // If it's an opening bracket, push the corresponding closing bracket onto the stack
         if (map[char]) {
             stack.push(map[char]);
         } else {
+            // If it's a closing bracket, check if it matches the last opened bracket
             if (stack.pop() !== char) {
                 return false;
             }
         }
     }
+
+    // If the stack is empty, all brackets were matched correctly
     return stack.length === 0;
 };
 
