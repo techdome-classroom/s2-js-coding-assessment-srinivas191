fef2b9de88ab01ee7b8db0c1f3c9dcbc4d84389b
srinivas191 <142072902+srinivas191@users.noreply.github.com>
2024-11-11 15:38:33 +0530

    solved

--- program1.js
+++ program1.js
@@ -3,9 +3,22 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-    
+    const stack = [];
+    const map = {
+        '(': ')',
+        '{': '}',
+        '[': ']'
+    };
+    for (let char of s) {
+        if (map[char]) {
+            stack.push(map[char]);
+        } else {
+            if (stack.pop() !== char) {
+                return false;
+            }
+        }
+    }
+    return stack.length === 0;
 };
 
 module.exports = { isValid };
-
-
